4572ae4e03ea1a7d6345391f181b13a1291e8535
preetikrp <preetikrp@gmail.com>
2017-07-16 22:09:23 -0500

    Family Dictionaly Exercise

--- The Family Dictionary/Program.cs
+++ The Family Dictionary/Program.cs
@@ -8,8 +8,55 @@ namespace The_Family_Dictionary
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){
+                { "name", "Krista"},
+                { "age", "42"}
+            });
+
+            myFamily.Add("Husband", new Dictionary<string, string>() {
+
+                {"name", "Amit Kumar" },
+                {"age", "40" }
+
+            });
+
+            myFamily.Add("daughter", new Dictionary<string, string>()
+            {
+
+                {"name", "Shreeti Amit" },
+                {"age", "13" }
+            });
+
+
+
+            //var qry = from outer in allOffset
+            //from inner in outer.Value
+            //select new { OuterKey = outer.Key, InnerKey = inner.Key, inner.Value };
+            /*
+            var nestedDictionary = from outer in myFamily
+                                   from inner in outer.Value
+                                   select new { OuterKey = outer.Key, InnerKey = inner.Key, InnerValue = inner.Value };
+            foreach (var nest in nestedDictionary)
+            {
+               // Console.WriteLine(nest.OuterKey);
+               // Console.WriteLine(nest.InnerKey);
+               //Console.WriteLine(nest.InnerValue);
+                Console.WriteLine($"{nest.OuterKey}" + ", " + $"{nest.InnerKey}" + ", " + $"{nest.InnerValue}");
+            }
+            */
+            
+            foreach (KeyValuePair<string, Dictionary<string, string>> nest in myFamily)
+            {
+                Console.WriteLine($"{nest.Value["name"]} is my {nest.Key} and is {nest.Value["age"]} years old");
+            }
+
+            Console.ReadKey();
         }
     }
 }
